dc1e2df7a1f2838d479b3d8f73804c7e4b8443c3
lyrl <lyrl@me.com>
2016-11-22 10:00:16 +0800

    * 快递100接口请求增加超时时间10秒

--- package_tracking/component/kuaidi100_compoent.py
+++ package_tracking/component/kuaidi100_compoent.py
@@ -40,21 +40,18 @@ class Kuaidi100ComponentImpl(Kuaidi100Component):
 
         trk_qry_api = 'https://sp0.baidu.com/9_Q4sjW91Qh3otqbppnN2DJv/pae/channel/data/asyncqury'
 
-        data = {
-            'appid': 4001,
-            'nu': trk_no
-        }
-
         opener = urllib2.build_opener()
         opener.addheaders = [
             ('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'),
             ('Cookie', 'BAIDUID=69B6916EE6792F0104B440DA366D541E:FG=1')
         ]
-
-        encoded_data = urllib.urlencode(data)
+        encoded_data = urllib.urlencode({
+            'appid': 4001,
+            'nu': trk_no
+        })
 
         try:
-            resp = opener.open(trk_qry_api, encoded_data).read()
+            resp = opener.open(trk_qry_api, encoded_data, timeout=10).read()
             logger.debug("[Kuaidi100] - 返回信息 %s " % resp)
             return json.loads(resp)
         except Exception as e:
